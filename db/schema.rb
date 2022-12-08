2cb35f659a159828ee146c5aa81e0e13c76c2dd9
Gideon <gideons564@gmail.com>
2022-12-08 12:29:51 +0100

    Resolve conflict in Schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_05_152341) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_06_082424) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_05_152341) do
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_cars_on_user_id"
+  end
+
+  create_table "rentals", force: :cascade do |t|
+    t.string "car_brand"
+    t.date "rent_date"
+    t.integer "number_of_days"
+    t.string "location"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
