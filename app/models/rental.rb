79c74e6a9451e3d9717faa614e55a023d20c85cd
Gideon <gideons564@gmail.com>
2022-12-09 13:39:52 +0100

    Correct linter errors

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,7 +1,7 @@
 class Rental < ApplicationRecord
   belongs_to :user, class_name: 'User', foreign_key: 'user_id'
 
-   validates :rent_date, presence: true
-    validates :number_of_days, presence: true
-    validates :location, presence: true
+  validates :rent_date, presence: true
+  validates :number_of_days, presence: true
+  validates :location, presence: true
 end
--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require 'test_helper'
 
 class RentalsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
